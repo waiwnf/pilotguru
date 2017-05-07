b431e25e4e2163c28cceb5098396c1f94b31d3c4
Anton Chechetka <chechetka@gmail.com>
2017-05-07 21:41:47 +0300

    Updated comments for the time series merging helper.

--- include/interpolation/align_time_series.hpp
+++ include/interpolation/align_time_series.hpp
@@ -9,28 +9,21 @@ namespace pilotguru {
 
 // A helper for merging multiple time series with non-aligned timestamps.
 //
-// The resulting merged time series has measurements at each of the components'
-// timestamps (i.e. the set of merged series timestamps is a union of
-// components' timestamps). The corresponding merged time series value is the
-// vector of the most recent values of component time series for that given
-// timestamp.
-//
-// Example: inputs (time, value)
-// (1, 2.0), (3, 4.0), (4, 5.0), (6, 7.0), (7, 9.0)
-// (2, 12.0), (3, 13.0), (4, 14.0), (5, 15.0) (6, 16.0)
+// Takes in one vector of timestamps per component time series.
+// Produces a vector, where each element is a vector of indices into the
+// corresponding component time series vector. The effective timestamp of every
+// result element is the latest timestamp among the component time series.
+
+// Example: two input time series with timestamps
+// 1, 3, 4, 6, 7
+// 2, 3, 4, 5  6
 //
 // Merged result:
-// (2, (2.0, 12.0)) - start at 2, don't know the value of sequence 2 at time 1.
-// (3, (4.0, 13.0))
-// (4, (5.0, 14.0))
-// (5, (5.0, 15.0)) - seq. 1 has no value at 5, use the most recent value at 4.
-// (6, (7.0, 16.0)) - end at 6, seq. 2 ends at 6, don't know the values after.
-//
-// Input: timestamps of several independent time series. each as a vector of
-// long.
-// Outputs: vectors indices into the each individual components for every
-// timestamp of the merged sequence.
-//
+// (0,0)  (effective time 2)
+// (1,1)  (effective time 3)
+// (2,2)  (effective time 4)
+// (2,3)  (effective time 5)
+// (3,4)  (effective time 6)
 std::vector<std::vector<size_t>>
 MergeTimeSeries(const std::vector<const std::vector<long> *> &in_timestamps);
 
