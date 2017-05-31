def2eed0d43e79311b4cc912ea14a40b6b26366d
Anton Chechetka <chechetka@gmail.com>
2017-05-31 12:40:04 +0300

    Added batchnorms to the models framework and the toy steering model.

--- python/models.py
+++ python/models.py
@@ -10,6 +10,13 @@ def ConvOutShape(in_shape, kernel_size, stride=1, padding=0, dilation=1):
   return [
       ConvOutSize(x, kernel_size, stride, padding, dilation) for x in in_shape]
 
+def TotalElements(shape):
+  assert len(shape) > 0
+  result = 1
+  for x in shape:
+    result = result * x
+  return result
+
 def MakeConv2d(in_shape, out_channels, kernel_size):
   in_channels = in_shape[0]
   out_shape = [out_channels] + ConvOutShape(in_shape[1:], kernel_size)
@@ -32,11 +39,13 @@ def MakeLinear(in_shape, out_size):
 
 def MakeFlatten(in_shape):
   assert len(in_shape) > 0
-  out_size = 1
-  for x in in_shape:
-    out_size *= x
+  out_size = TotalElements(in_shape)
   layer = lambda x : x.view(-1, out_size)
   return layer, [out_size]
+
+def MakeBatchNorm2d(in_shape):
+  assert len(in_shape) == 3
+  return nn.BatchNorm2d(in_shape[0]), in_shape
 
 class SequentialNet(nn.Module):
   def __init__(self, in_shape):
@@ -61,6 +70,9 @@ class SequentialNet(nn.Module):
   
   def AddFlatten(self):
     return self.AddLayer(MakeFlatten(self.OutShape()))
+  
+  def AddBatchNorm2d(self):
+    return self.AddLayer(MakeBatchNorm2d(self.OutShape()))
 
   def AddLayer(self, layer_tuple):
     layer, out_shape = layer_tuple
@@ -82,19 +94,24 @@ class ToyConvNet(SequentialNet):
     super(ToyConvNet, self).__init__(in_shape)
 
     self.conv1 = self.AddConv2d(6, 5)
+    self.c1bn = self.AddBatchNorm2d()
     self.AddRelu()
     self.pool1 = self.AddMaxPool2d(2)
 
     self.conv2 = self.AddConv2d(16, 5)
+    self.c2bn = self.AddBatchNorm2d()
     self.AddRelu()
     self.pool2 = self.AddMaxPool2d(2)
 
     self.conv3 = self.AddConv2d(1, 5)
+    self.c3bn = self.AddBatchNorm2d()
     self.AddRelu()
     self.pool3 = self.AddMaxPool2d(2)
 
     self.AddFlatten()
 
     self.fc1 = self.AddLinear(120)
+    self.AddRelu()
     self.fc2 = self.AddLinear(84)
+    self.AddRelu()
     self.fc3 = self.AddLinear(1)
